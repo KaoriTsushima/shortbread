61d71b7df120f1a05c80bc11cf238f1029bf7cc1
Kaori Tsushima <kaoritsushima0107@gmail.com>
2024-09-24 16:16:37 +0100

    created a router link

--- src/app/shortbread-details/shortbread-details.component.ts
+++ src/app/shortbread-details/shortbread-details.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { ShortbreadDetails } from '../shortbread-details';
 import { NzRateModule } from 'ng-zorro-antd/rate';
 import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shortbread-details',
   standalone: true,
-  imports: [FormsModule, NzRateModule],
+  imports: [FormsModule, NzRateModule, RouterLink, RouterOutlet],
   template: `
     <section class="shortbread-list">
       <h2>{{ shortbreadDetails.name }}</h2>
@@ -19,8 +20,11 @@ import { FormsModule } from '@angular/forms';
         [ngModel]="shortbreadDetails.rating"
         nzAllowHalf
       ></nz-rate>
+      <br />
+      <a [routerLink]="['/details', shortbreadDetails.id]">See nutrient data</a>
     </section>
   `,
+
   styleUrl: './shortbread-details.component.css',
 })
 export class ShortbreadDetailsComponent {
