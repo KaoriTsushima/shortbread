505d08a2733bf0a493bdf9de73f0fd8a127cc1c9
Kaori Tsushima <kaoritsushima0107@gmail.com>
2024-09-27 11:34:57 +0100

    added home icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterOutlet } from '@angular/router';
 import { ShortbreadDetailsComponent } from './shortbread-details/shortbread-details.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NzIconModule, provideNzIconsPatch } from 'ng-zorro-antd/icon';
+import { HomeOutline } from '@ant-design/icons-angular/icons';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +16,9 @@ import { HomeComponent } from './home/home.component';
     CommonModule,
     RouterModule,
     HomeComponent,
+    NzIconModule,
   ],
+  providers: [provideNzIconsPatch([HomeOutline])],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
